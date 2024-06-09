902afa1cc08b301cb665bde6e1ccc8f8b657b27c
kedarlin <kedarkamaf@gmail.com>
2024-06-09 10:56:36 +0530

    inited backend

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/task', taskRoutes);
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.send('Hello');
 });
 // Start the server
